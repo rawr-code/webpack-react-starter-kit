39dd10a9d53926c8397dce0d4b9a215a9fd1d3ce
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-06-18 19:39:33 -0400

    fix sass and add node-sass

--- webpack/loaders/css.js
+++ webpack/loaders/css.js
@@ -1,9 +1,6 @@
 module.exports = {
   loader: 'css-loader',
   options: {
-    sourceMap: true,
-    modules: true,
-    importLoader: 1,
-    localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+    sourceMap: true
   }
 };
